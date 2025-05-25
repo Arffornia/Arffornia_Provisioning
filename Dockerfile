6ad208ed59ce21d9be9c38660be3e951cb4640a9
brian.perret <brian.perret@epita.fr>
2025-05-25 14:11:27 +0200

    feat: Add mod dir name

--- Dockerfile
+++ Dockerfile
@@ -11,5 +11,6 @@ RUN npm run build
 ENV INSTALL_BASE_PATH="/data"
 ENV BASE_URL="https://raw.githubusercontent.com/Arffornia/Arffornia_Network/refs/heads/main/"
 ENV KIND="server"
+ENV MODS_DIR_NAME="mods"
 
-CMD ["sh", "-c", "node dist/configUpdater.js \"$INSTALL_BASE_PATH\" \"$BASE_URL\" \"$KIND\""]
+CMD ["sh", "-c", "node dist/configUpdater.js \"$INSTALL_BASE_PATH\" \"$BASE_URL\" \"$KIND\" \"$MODS_DIR_NAME\""]
--- src/configUpdater.ts
+++ src/configUpdater.ts
@@ -6,12 +6,13 @@ import { exit } from "process";
  * @param {string} installBasePath 
  * @param {string} baseUrl 
  * @param {string} kind 
+ * @param {string} modDirName
  */
-async function updateConfig(installBasePath: string, baseUrl: string, kind: string): Promise<void> {
+async function updateConfig(installBasePath: string, baseUrl: string, kind: string, modDirName: string): Promise<void> {
     const externalFilesUrl = new URL(`config/${kind}/`, baseUrl).href;
     const modListUrl = new URL(`config/${kind}/modList.json`, baseUrl).href;
 
-    const nexusMods = new NexusMods(installBasePath);
+    const nexusMods = new NexusMods(installBasePath, callback, modDirName);
 
     try {
         await Promise.all([
@@ -28,17 +29,27 @@ async function updateConfig(installBasePath: string, baseUrl: string, kind: stri
     }
 }
 
+const callback = {
+    onStep(step) {
+        console.log(step);
+    },
+    onProgress(totalDownloaded, totalToDownload, name) {
+        console.log(`totalDownloaded: ${totalDownloaded} | totalToDownload: ${totalToDownload} | name: ${name}`);
+    },
+
+};
+
 async function main() {
     const args = process.argv.slice(2);
 
-    if (args.length !== 3) {
-        console.info("Usage: installBasePath baseUrl kind");
+    if (args.length !== 4) {
+        console.info("Usage: installBasePath baseUrl kind modDirName");
         exit(1);
     }
 
-    const [ installBasePath, baseUrl, kind ] = args;
+    const [ installBasePath, baseUrl, kind, modDirName ] = args;
 
-    await updateConfig(installBasePath, baseUrl, kind);
+    await updateConfig(installBasePath, baseUrl, kind, modDirName);
 }
 
 main();
